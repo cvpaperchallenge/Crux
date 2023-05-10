935f6802ae2e3e3a873c38a52869ff29d01154dc
botany <yoshikikubotani.lab@gmail.com>
2023-05-11 08:27:19 +0900

    :sparkles: Add an endpoint for pdf summarization

--- applications/backend/src/routers.py
+++ applications/backend/src/routers.py
@@ -1,8 +1,8 @@
-from typing import Final
+from typing import Final, Literal
 
 import fastapi
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, SecretStr
 
 router: Final = fastapi.APIRouter()
 
@@ -10,6 +10,13 @@ router: Final = fastapi.APIRouter()
 class Health(BaseModel):
     health: str
 
+class UserIn(BaseModel):
+    parsed_pdf: dict
+    openai_key: SecretStr
+    summary_type: Literal['normal', 'ochiai']
+
+class UserOut(BaseModel):
+    summary: str
 
 @router.get("/health", response_model=Health)
 async def health() -> JSONResponse:
@@ -22,3 +29,15 @@ async def health() -> JSONResponse:
 
     """
     return JSONResponse({"health": "ok"})
+
+@router.post("/summarize", response_model=UserOut)
+async def summarize(user_input: UserIn) -> JSONResponse:
+    """Endpoint for summarizing paper PDFs.
+
+    Returns:
+
+    - JSONResponse: A response from endpoint.
+
+
+    """
+    return JSONResponse({"summary": "This is a summary"})
