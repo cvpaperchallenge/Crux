35e56e9ed683ca103c1707322285fdef385172cd
botany <yoshikikubotani.lab@gmail.com>
2023-05-29 01:57:05 +0900

    :test_tube: Update test code

--- applications/backend/tests/src/test_endpoint.py
+++ applications/backend/tests/src/test_endpoint.py
@@ -32,11 +32,35 @@ def test_summarize_1(
 
         response = dummy_client.post(request_url, files=files)
         assert response.status_code == http.HTTPStatus.OK
+        if summary_format == "normal":
+            summary_text = {"summary": "This is a normal summary."}
+        elif summary_format == "three-point":
+            summary_text = {
+                "first_point": "This is the first point.",
+                "second_point": "This is the second point.",
+                "third_point": "This is the third point.",
+            }
+        elif summary_format == "ochiai":
+            summary_text = {
+                "abstract": "This is the abstract.",
+                "strength_over_previous_study": "This is the strength over previous study.",
+                "method": "This is the method.",
+                "evaluation": "This is the evaluation.",
+                "discussion": "This is the discussion.",
+                "next_paper": "This is the next paper.",
+            }
+        elif summary_format == "cvpaper":
+            summary_text = {
+                "abstract": "This is the abstract.",
+                "novelty": "This is the novelty.",
+                "results": "This is the results.",
+                "others": "This is the others.",
+            }
         assert response.json() == {
             "title": "This is the paper title.",
             "author": "John Doe",
             "key_figure": "fig-#",
-            "summary_text": "This is a {} summary.".format(summary_format),
+            "summary_text": summary_text,
         }
 
 
@@ -53,9 +77,33 @@ def test_summarize_2(
 
         response = dummy_client.post("/summarize_2", files=files, data=data)
         assert response.status_code == http.HTTPStatus.OK
+        if summary_format == "normal":
+            summary_text = {"summary": "This is a normal summary."}
+        elif summary_format == "three-point":
+            summary_text = {
+                "first_point": "This is the first point.",
+                "second_point": "This is the second point.",
+                "third_point": "This is the third point.",
+            }
+        elif summary_format == "ochiai":
+            summary_text = {
+                "abstract": "This is the abstract.",
+                "strength_over_previous_study": "This is the strength over previous study.",
+                "method": "This is the method.",
+                "evaluation": "This is the evaluation.",
+                "discussion": "This is the discussion.",
+                "next_paper": "This is the next paper.",
+            }
+        elif summary_format == "cvpaper":
+            summary_text = {
+                "abstract": "This is the abstract.",
+                "novelty": "This is the novelty.",
+                "results": "This is the results.",
+                "others": "This is the others.",
+            }
         assert response.json() == {
             "title": "This is the paper title.",
             "author": "John Doe",
             "key_figure": "fig-#",
-            "summary_text": "This is a {} summary.".format(summary_format),
+            "summary_text": summary_text,
         }
