c723a2956d2dc0f15039587b7074ea63f3eef69f
botany <yoshikikubotani.lab@gmail.com>
2023-05-29 23:33:29 +0900

    :sparkles: Add field description to dto objects

--- applications/backend/src/dto.py
+++ applications/backend/src/dto.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import BaseModel, SecretStr
+from pydantic import BaseModel, SecretStr, Field
 
 
 class Health(BaseModel):
@@ -8,29 +8,29 @@ class Health(BaseModel):
 
 
 class FormatNormal(BaseModel):
-    summary: str
+    summary: str = Field(description="200-word summary of the paper in Japanese")
 
 
 class FormatThreePoint(BaseModel):
-    first_point: str
-    second_point: str
-    third_point: str
+    first_point: str = Field(description="First point of the three-point summary of the paper.")
+    second_point: str = Field(description="Second point of the three-point summary of the paper.")
+    third_point: str = Field(description="Third point of the three-point summary of the paper.")
 
 
 class FormatOchiai(BaseModel):
-    abstract: str
-    strength_over_previous_study: str
-    method: str
-    evaluation: str
-    discussion: str
-    next_paper: str
+    abstract: str = Field(description="Single-sentence summary of what this study did")
+    strength_over_previous_study: str = Field(description="What makes it better than previous studies?")
+    method: str = Field(description="The key to techniques and methods")
+    evaluation: str = Field(description="Evaluation methodology of the proposed method")
+    discussion: str = Field(description="Discussion on the results of the evaluation")
+    next_paper: str = Field(description="Papers recommneded to read next")
 
 
 class FormatCVPaper(BaseModel):
-    abstract: str
-    novelty: str
-    results: str
-    others: str
+    abstract: str = Field(description="Single-sentence summary of what this study did")
+    novelty: str = Field(description="Novelty of this paper")
+    results: str = Field(description="Result of the paper")
+    others: str = Field(description="Anything else, if any")
 
 
 class UserIn(BaseModel):
