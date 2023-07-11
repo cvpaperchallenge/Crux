5d32c34b49767ed005bf19b44f52b850b5901c96
botany <yoshikikubotani.lab@gmail.com>
2023-07-12 00:21:58 +0900

    :sparkles: Update to process one file

--- applications/backend/src/adapter/summary_controller.py
+++ applications/backend/src/adapter/summary_controller.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Final
+import logging
 import pathlib
 
 from fastapi import UploadFile
@@ -6,15 +7,33 @@ from pydantic import SecretStr
 
 from src.adapter.rdb_repository_gateway import RDBRepositoryGateway
 from src.usecase.paper_io_handler import PaperIOHandler
+from src.usecase.mathpix_pdf_parser import MathpixPdfParser
+
+logger: Final = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 class SummaryController:
-    def __init__(self, paper_repository: RDBRepositoryGateway, summary_repository: RDBRepositoryGateway, static_files_storage_root: pathlib.Path) -> None:
+    def __init__(
+            self,
+            paper_repository: RDBRepositoryGateway,
+            summary_repository: RDBRepositoryGateway,
+            static_files_storage_root: pathlib.Path,
+            mathpix_api_key: SecretStr,
+            mathpix_api_id: SecretStr,
+        ) -> None:
         self.paper_repository = paper_repository
         self.summary_repository = summary_repository
         self.iohandler = PaperIOHandler(static_files_storage_root)
+        self.pdf_parser = MathpixPdfParser(mathpix_api_key, mathpix_api_id)
 
-    def summarize(self, pdf_files: list[UploadFile], openai_key: SecretStr, mathpix_key: SecretStr) -> dict[str, Any]:
-        # Save pdf files to the storage
-        self.iohandler.save_pdf(pdf_files)
-        # parse pdf file to latex form and save it to storage
-        # parsed_pdf = mathpix_handler(self.paper_repository).parse_pdf(pdf_file)
+    def summarize(self, pdf_files: list[UploadFile], openai_api_key: SecretStr) -> dict[str, Any]:
+        
+        for i, pdf_file in enumerate(pdf_files):
+            
+            logger.info(f"[{i+1}/{len(pdf_files)}] Start Processing `{pdf_file.filename}`.")
+            # Save pdf files to the storage
+            pdf_file_path = self.iohandler.save_pdf(pdf_file)
+
+            # parse pdf file to latex form and save it to storage
+            pdf_text = self.pdf_parser.load_pdf(pdf_file_path)
+            parsed_pdf = self.pdf_parser.parse_pdf(pdf_text)
--- applications/backend/src/usecase/paper_io_handler.py
+++ applications/backend/src/usecase/paper_io_handler.py
@@ -11,23 +11,25 @@ class PaperIOHandler():
     def __init__(self, root_path: pathlib.Path) -> None:
         self.root_path = root_path
 
-    def save_pdf(self, pdf_files: list[UploadFile]) -> None:
+    def save_pdf(self, pdf_file: UploadFile) -> pathlib.Path:
         """Save pdf file to the storage.
 
         Args:
-            pdf_file list[UploadFile]: pdf file to save
+            pdf_file UploadFile: pdf file to save
 
         """
-        for i, pdf_file in enumerate(pdf_files):
-            directory_path = self.root_path / pdf_file.filename.removesuffix(".pdf")
-            file_path = directory_path / pdf_file.filename
-            logger.info(f"[{i+1}/{len(pdf_files)}] Saving paper `{pdf_file.filename}`.")
-            
-            # If directory already exists, skip it.
-            if directory_path.exists():
-                continue
-            
-            # Create directory to save PDF.
+        directory_path = self.root_path / str(pdf_file.filename).removesuffix(".pdf")
+        file_path = directory_path / str(pdf_file.filename)
+        
+        # If directory already exists, skip it.
+        if directory_path.exists():
+            logger.info(f"`{str(directory_path)}` already exists.")
+        
+        # If else, create directory to save PDF.
+        else:
+            logger.info(f"Saving paper `{pdf_file.filename}`.")
             directory_path.mkdir(parents=True)
             with open(file_path, "wb") as f:
                 f.write(pdf_file.file.read())
+    
+        return file_path
