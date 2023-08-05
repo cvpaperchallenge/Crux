d4c55b05e8f1ee9d2cb2ec59ff233076c3b72174
botany <yoshikikubotani.lab@gmail.com>
2023-08-06 02:22:47 +0900

    :speech_balloon: Comment out the old test code

--- applications/backend/tests/conftest.py
+++ applications/backend/tests/conftest.py
@@ -1,43 +1,43 @@
-import pathlib
+# import pathlib
 
-import pytest
+# import pytest
 
-sample_pdf_path = pathlib.Path("./tests/samples/sample_paper.pdf")
-sample_openai_key = "DUMMY_OPENAI_KEY"
-sample_summary_format_list = ["normal", "three-point", "ochiai", "cvpaper"]
-
-
-@pytest.fixture
-def pdf_factory():
-    def _pdf_factory(pdf_path: pathlib.Path = sample_pdf_path) -> str:
-        return pdf_path.open("rb")
-
-    return _pdf_factory
+# sample_pdf_path = pathlib.Path("./tests/samples/sample_paper.pdf")
+# sample_openai_key = "DUMMY_OPENAI_KEY"
+# sample_summary_format_list = ["normal", "three-point", "ochiai", "cvpaper"]
 
 
-@pytest.fixture(scope="session")
-def openai_key_factory():
-    def _openai_key_factory(openai_key: str = sample_openai_key) -> str:
-        return openai_key
+# @pytest.fixture
+# def pdf_factory():
+#     def _pdf_factory(pdf_path: pathlib.Path = sample_pdf_path) -> str:
+#         return pdf_path.open("rb")
 
-    return _openai_key_factory
-
-
-@pytest.fixture(scope="session")
-def summary_format_list_factory():
-    def _summary_format_list_factory(
-        summary_format_list: list[str] = sample_summary_format_list,
-    ) -> list[str]:
-        return summary_format_list
-
-    return _summary_format_list_factory
+#     return _pdf_factory
 
 
-@pytest.fixture(scope="session")
-def summary_input_factory():
-    def _summary_input_factory(
-        summary_format: str, openai_key: str = sample_openai_key
-    ) -> dict[str, str]:
-        return {"openai_key": openai_key, "summary_format": summary_format}
+# @pytest.fixture(scope="session")
+# def openai_key_factory():
+#     def _openai_key_factory(openai_key: str = sample_openai_key) -> str:
+#         return openai_key
 
-    return _summary_input_factory
+#     return _openai_key_factory
+
+
+# @pytest.fixture(scope="session")
+# def summary_format_list_factory():
+#     def _summary_format_list_factory(
+#         summary_format_list: list[str] = sample_summary_format_list,
+#     ) -> list[str]:
+#         return summary_format_list
+
+#     return _summary_format_list_factory
+
+
+# @pytest.fixture(scope="session")
+# def summary_input_factory():
+#     def _summary_input_factory(
+#         summary_format: str, openai_key: str = sample_openai_key
+#     ) -> dict[str, str]:
+#         return {"openai_key": openai_key, "summary_format": summary_format}
+
+#     return _summary_input_factory
